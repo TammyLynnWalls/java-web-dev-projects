208de104c29788f414631fbe414fccf7efe70031
Tammy <tammylwalls@gmail.com>
2023-11-05 21:31:34 -0600

    completed authentication chapter

--- codingevents/src/main/java/org/launchcode/codingevents/models/Event.java
+++ codingevents/src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,48 +1,59 @@
 package org.launchcode.codingevents.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 public class Event extends AbstractEntity{
 //    @Id
 //    @GeneratedValue
 //    private int id;
+
+
+
     @Size(min=3, max=50, message = "Name must be between 3 and 50 characters.")
     @NotBlank (message="Name is required")
     private String name;
-    @Size(max=500, message = "Description too long!")
-    private String description;
-    @NotBlank (message="email is required.")
-    @Email(message = "Invalid email. Try again.")
-    private String contactEmail;
+    @OneToOne(cascade = CascadeType.ALL)
+    @Valid
     @NotNull
-    @NotBlank (message="Location cannot be left blank")
-    private String location;
-    @AssertTrue(message="Registration is required")
-    private boolean registrationRequired;
-    @Positive(message="Number of attendees must be one or more")
-    private int numberOfAttendees;
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
-    @Future (message="Event must be in the future.")
-    private Date dateOfEvent;
+    private EventDetails eventDetails;
+//    @Size(max=500, message = "Description too long!")
+//    private String description;
+//    @NotBlank (message="email is required.")
+//    @Email(message = "Invalid email. Try again.")
+//    private String contactEmail;
+//    @NotNull
+//    @NotBlank (message="Location cannot be left blank")
+//    private String location;
+//    @AssertTrue(message="Registration is required")
+//    private boolean registrationRequired;
+//    @Positive(message="Number of attendees must be one or more")
+//    private int numberOfAttendees;
+//    @DateTimeFormat(pattern = "yyyy-MM-dd")
+//    @Future (message="Event must be in the future.")
+//    private Date dateOfEvent;
 
     @ManyToOne
     @NotNull(message = "Category is required")
     private EventCategory eventCategory;
 
-    public Event(String name, String description, String contactEmail, EventCategory eventCategory) {
+    @ManyToMany
+    private final List<Tag> tags = new ArrayList<>();
+    public Event(String name, EventCategory eventCategory) {
         this.name = name;
-        this.description = description;
-        this.contactEmail = contactEmail;
-        this.location = location;
-        this.registrationRequired = registrationRequired;
-        this.numberOfAttendees = numberOfAttendees;
-        this.dateOfEvent = dateOfEvent;
+//        this.description = description;
+//        this.contactEmail = contactEmail;
+//        this.location = location;
+//        this.registrationRequired = registrationRequired;
+//        this.numberOfAttendees = numberOfAttendees;
+//        this.dateOfEvent = dateOfEvent;
         this.eventCategory = eventCategory;
 
     }
@@ -58,21 +69,28 @@ public class Event extends AbstractEntity{
 
     }
 
-    public String getDescription() {
-        return description;
+    public EventDetails getEventDetails() {
+        return eventDetails;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setEventDetails(EventDetails eventDetails) {
+        this.eventDetails = eventDetails;
     }
-
-    public String getContactEmail() {
-        return contactEmail;
-    }
-
-    public void setContactEmail(String contactEmail) {
-        this.contactEmail = contactEmail;
-    }
+    //    public String getDescription() {
+//        return description;
+//    }
+//
+//    public void setDescription(String description) {
+//        this.description = description;
+//    }
+//
+//    public String getContactEmail() {
+//        return contactEmail;
+//    }
+//
+//    public void setContactEmail(String contactEmail) {
+//        this.contactEmail = contactEmail;
+//    }
 
     public EventCategory getEventCategory() {
         return eventCategory;
@@ -82,40 +100,50 @@ public class Event extends AbstractEntity{
         this.eventCategory = eventCategory;
     }
 
-    public String getLocation() {
-        return location;
+//    public String getLocation() {
+//        return location;
+//    }
+//
+//    public void setLocation(String location) {
+//        this.location = location;
+//    }
+//
+//    public boolean isRegistrationRequired() {
+//        return registrationRequired;
+//    }
+//
+//    public void setRegistrationRequired(boolean registrationRequired) {
+//        this.registrationRequired = registrationRequired;
+//    }
+//
+//    public int getNumberOfAttendees() {
+//        return numberOfAttendees;
+//    }
+//
+//    public void setNumberOfAttendees(int numberOfAttendees) {
+//        this.numberOfAttendees = numberOfAttendees;
+//    }
+//
+//    public Date getDateOfEvent() {
+//        return dateOfEvent;
+//    }
+//
+//    public void setDateOfEvent(Date dateOfEvent) {
+//        this.dateOfEvent = dateOfEvent;
+//    }
+//
+
+
+    public List<Tag> getTags() {
+        return tags;
     }
 
-    public void setLocation(String location) {
-        this.location = location;
+
+
+    public void addTag(Tag tag) {
+        this.tags.add(tag);
+
     }
-
-    public boolean isRegistrationRequired() {
-        return registrationRequired;
-    }
-
-    public void setRegistrationRequired(boolean registrationRequired) {
-        this.registrationRequired = registrationRequired;
-    }
-
-    public int getNumberOfAttendees() {
-        return numberOfAttendees;
-    }
-
-    public void setNumberOfAttendees(int numberOfAttendees) {
-        this.numberOfAttendees = numberOfAttendees;
-    }
-
-    public Date getDateOfEvent() {
-        return dateOfEvent;
-    }
-
-    public void setDateOfEvent(Date dateOfEvent) {
-        this.dateOfEvent = dateOfEvent;
-    }
-
-
-
     @Override
     public String toString() {
         return name;
